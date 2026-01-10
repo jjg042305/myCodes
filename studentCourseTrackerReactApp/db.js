b310a24b32f52a5763b2acb4ca9d2d2d7d320f2f
JJ <jj.gougassian.jr@gmail.com>
2026-01-10 14:54:45 +0400

    Revert "Update db.js"
    This reverts commit d99bf803ab4325d40ed74acea80367ef72207ae4.

--- studentCourseTrackerReactApp/db.js
+++ studentCourseTrackerReactApp/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'mypassword'
+    password: 'Lego2015'
 
 });
 
