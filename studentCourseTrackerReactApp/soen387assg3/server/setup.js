58095017b7345e6b134d9d4dc513954fc073b057
jjg042305 <jj.gougassian.jr@gmail.com>
2026-01-10 13:19:24 +0200

    Update setup.js

--- studentCourseTrackerReactApp/soen387assg3/server/setup.js
+++ studentCourseTrackerReactApp/soen387assg3/server/setup.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Lego2015',
+    password: 'mypassword',
      multipleStatements: true
 
 });
