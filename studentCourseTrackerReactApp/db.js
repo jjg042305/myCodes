d99bf803ab4325d40ed74acea80367ef72207ae4
jjg042305 <jj.gougassian.jr@gmail.com>
2026-01-10 12:46:18 +0200

    Update db.js

--- studentCourseTrackerReactApp/db.js
+++ studentCourseTrackerReactApp/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Lego2015'
+    password: 'mypassword'
 
 });
 
