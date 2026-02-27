bb11f2cb554638cde1df2a03d40981cd934af262
JJ <jj.gougassian.jr@gmail.com>
2026-02-27 19:13:58 +0400

    remove student course tracker and cats and dogs management

--- catsAndDogsManagement/public/displayTime.js
+++
@@ -1,8 +0,0 @@
-function displayTime() {
-    const d = new Date();
-    const dateTimeString = d.toLocaleString();
-
-    document.getElementById("date").innerHTML = dateTimeString;
-}
-displayTime();
-setInterval(displayTime, 1000);
